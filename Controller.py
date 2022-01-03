988d5fa72a76061e2e4dade00449d3a6aa64eca2
Jorge Salas <jasalas@alumni.uncg.edu>
2022-01-02 22:30:58 -0500

    added a keyboard mode for faster interaction with ethe cube by pressing tab, the functionality to undo, reset, reload, and execute have been moved to the [,-,=, and ] keys

--- Controller.py
+++ Controller.py
@@ -8,35 +8,54 @@ class Controller:
 		self._draw()
 		
 	def _assign_callbacks(self):
-		commands =  {"Up":self.reload, 
-		             "Down":self.reset, 
-		             "Left":self.undo,
-		             "Right":self.execute, 
-		             "Return": self.execute}
-		             
+		commands =  {"equal":self.reload, 
+		             "minus":self.reset, 
+		             "bracketleft":self.undo,
+		             "bracketright":self.execute, 
+		             "Return": self.execute,
+		             "w":lambda: self.execute("B"),
+		             "e":lambda: self.execute("D"),
+		             "r":lambda: self.execute("R"),
+		             "s":lambda: self.execute("B'"),
+		             "d":lambda: self.execute("D'"),
+		             "f":lambda: self.execute("R'"),
+		             "Tab":self.toggle_keyboard_mode}
 		self.view["parent"].bind("<Key>", lambda e: commands[e.keysym]() if e.keysym in commands else print(e))
 		self.view["reset_button"]["command"] = self.reset
 		self.view["execute_button"]["command"] = self.execute
 		self.view["undo_button"]["command"] = self.undo
 		self.view["reload_button"]["command"] = self.reload
 	
+	def toggle_keyboard_mode(self):	
+		if str(self.view["canvas"].focus_get()) == ".!entry":
+			self.view["canvas"].focus()
+			self.view["statusbar"]["text"] = "keyboard mode off, click back on entry to turn on"
+	
 	def reset(self):
 		print(self.model.reset())
 		self._draw()
 	
-	def execute(self):
+	def execute(self, value = None): 
+		keyboard_mode = str(self.view["canvas"].focus_get()) != ".!entry"
+		print(str(self.view["canvas"].focus_get()), keyboard_mode, value)
+		if not keyboard_mode and value != None:
+			return
+			
 		try:
 			instructions = self.view["data"].get()
-			print(self.model.execute(instructions))
-			self.view["statusbar"]["text"] = "executed {:s}".format(instructions)
+			print(self.model.execute(value if keyboard_mode else instructions))
+			print(self.model.get_recent_operations())
+			self.view["statusbar"]["text"] = "executed {:s}".format(value if keyboard_mode else instructions)
 		except:
 			self.view["statusbar"]["text"] = "failed to execute instructions!"
 		self._draw()
 	
 	def undo(self):
 		print(self.model.undo())
+		print(self.model.get_recent_operations())
 		self._draw()
 		self.view["statusbar"]["text"] = "reverted to previous state"
+		
 		
 	def _draw(self):
 		self.view["canvas"].render(self.model["F"],top=True)
